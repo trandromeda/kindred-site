5f9155dc3e6c1ac3498375463ec662d8b495fa11
Andy Tran <transient.tran@gmail.com>
2018-01-28 16:05:13 -0500

    lots of design added, including dynamic and unique email bar

---
+++ src/components/EmailBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class EmailBar extends Component {
+  render() {
+    return (
+        <div className="email-bar">
+          <div className="left"></div>
+          
+          <div className="mid-section">
+            <div className="middle-top"></div>
+            <div className="input-box"></div>
+            <div className="middle-bottom"></div>
+          </div>
+
+          <div className="right"></div>
+
+        </div>
+      )
+  }
+}
+
+export default EmailBar;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import logo from '../images/colour-logo.png';
 
 class Navbar extends Component {
   render() {
     return (
         <div className="navbar">
-        Kindred
+          <img src={logo} alt="Logo" />
         </div>
       )
   }
