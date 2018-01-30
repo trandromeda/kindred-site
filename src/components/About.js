24ce35fd60be9d0052ba8b79e0f17f2786924df7
Andy Tran <transient.tran@gmail.com>
2018-01-30 15:38:34 -0500

    about page fixes

--- src/components/About.js
+++ src/components/About.js
@@ -4,38 +4,37 @@ class About extends Component {
   render() {
     return (
         <div className="about">
+            <h1>About</h1>
 
-        <h1>About</h1>
+            <p>
+            Kindred is a community for people to learn valuable life skills from retirees via small, in-person group lessons. Meet interesting folks, share stories and laughter with friendly faces from your neighbourhood, and come away with unforgettable new memories.
+            </p>
 
-        <p>
-        Kindred is a community for people to learn valuable life skills from retirees via small, in-person group lessons. Meet interesting folks, share stories and laughter with friendly faces from your neighbourhood, and come away with unforgettable new memories.
-        </p>
+            <p>
+            Founded in 2017 and based in Toronto, Kindred connects you with individuals from all walks of life to learn from one another while having fun, whether it's making Maria's secret spaghetti sauce, practicing Hubert's six easy steps to tie a tie, or packing colourful bento boxes with Hisami.
+            </p>
 
-        <p>
-        Founded in 2017 and based in Toronto, Kindred connects you with individuals from all walks of life to learn from one another while having fun, whether it's making Maria's secret spaghetti sauce, practicing Hubert's six easy steps to tie a tie, or packing colourful bento boxes with Hisami.
-        </p>
+            <h1>Mission</h1>
+            <p>
+            Our goal is to enrich the lives of seniors and empower them to be inspiring mentors for young people everywhere. We will do this by creating spaces for fun and personalized learning experiences, highlighting the wealth of knowledge that seniors have to share.
+            </p>
 
-        <h1>Mission</h1>
-        <p>
-        Our goal is to enrich the lives of seniors and empower them to be inspiring mentors for young people everywhere. We will do this by creating spaces for fun and personalized learning experiences, highlighting the wealth of knowledge that seniors have to share.
-        </p>
+            <p>
+            In doing so, we will also strive to:
+            </p>
 
-        <p>
-        In doing so, we will also strive to:
-        </p>
+            <ul>
+              <li>Preserve cultural knowledge that may otherwise be lost to time,</li>
+              <li>Encourage people to do social good,</li>
+              <li>Provide a form of financial gain to seniors, and</li>
+              <li>Tap into the underutilized resources possessed by an entire generation</li>
 
-        <ul>
-          <li>Preserve cultural knowledge that may otherwise be lost to time,</li>
-          <li>Encourage people to do social good,</li>
-          <li>Provide a form of financial gain to seniors, and</li>
-        <li>Tap into the underutilized resources possessed by an entire generation</li>
+            </ul>        
 
-        </ul>        
-
-        <h1>Vision</h1>
-        <p>
-        A world where every stage of life is celebrated and lived to its fullest, from beginning to end.
-        </p>
+            <h1>Vision</h1>
+            <p>
+            A world where every stage of life is celebrated and lived to its fullest, from beginning to end.
+            </p>
         </div>
       )
   }
