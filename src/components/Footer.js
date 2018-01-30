02602d75fa049010254fcdb5b4b1e346d19445a3
Andy Tran <transient.tran@gmail.com>
2018-01-30 11:30:11 -0500

    added dummy links to footer and styled the lesson cards some more, mix of flex and hard positioning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,22 @@ class Footer extends Component {
   render() {
     return (
         <div className="footer">
+          <h1>Kindred</h1>
 
+          <div className="column-1">
+            <a href="/">About</a>
+            <a href="/">Teach on Kindred</a>
+            <a href="/">Who we are</a>
+          </div>
+
+          <div className="column-2">
+            <a href="/">Blog</a>
+            <a href="/">Contact us</a>
+          </div>
+
+          <div className="divider"></div>       
+
+          <p className="copyright">Copyright 2018, Kindred Experiences Inc. All Rights Reserved</p>
         </div>
       )
   }
