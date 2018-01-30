65b52d7dfb62df20e5173c5dee4bc56649b06993
Andy Tran <transient.tran@gmail.com>
2018-01-30 14:10:42 -0500

    using react-router to render new pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './styles/App.css';
+import logo from './images/colour-logo.png';
 
-import Navbar from './components/Navbar';
 import Landing from './components/Landing';
-import LessonCard from './components/LessonCard';
-import EmailBar from './components/EmailBar';
+import Home from './components/Home';
+import About from './components/About';
 import Footer from './components/Footer';
 
 class App extends Component {
 
   componentDidMount() {
-    console.log('mounted');
     const nav = document.getElementById("navbar");
     window.onscroll = function() {
       if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
@@ -24,30 +24,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Landing />
-        <div className="preview">
-          <h1>Preview</h1>
-          <div className="lesson-container">
-            <LessonCard />
-            <LessonCard />
-            <LessonCard />
-            <LessonCard />
-            <LessonCard />
-            <LessonCard />          
-          </div>
-
-          <div className="preview-description">
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et ligula ligula. Nullam pharetra sapien eget magna semper accumsan. Pellentesque lobortis tincidunt gravida. Integer iaculis velit in augue pellentesque, vitae cursus nunc laoreet. Vivamus justo nulla, efficitur sed nulla id, tincidunt vehicula lectus. Nunc tempus rhoncus porttitor. Fusce vitae venenatis nunc. Sed ornare scelerisque neque ut euismod. Cras id scelerisque ipsum, eleifend euismod libero. Maecenas dignissim ut diam quis ornare. Duis vehicula vehicula imperdiet.
-            </p>
-          </div>
+        <div className="navbar" id="navbar">
+          <Link to="/"><img src={logo} alt="Logo" className="logo" /></Link>
+          <Link to="/about" className="about">About</Link>
+          <Link to="/teach" className="mentor">Mentor</Link>
+          <Link to="/browse" className="browse">Browse Lessons</Link>
         </div>
-
-        <EmailBar />
-
+        <Landing />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
         <Footer />
-
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import LessonCard from './LessonCard';
+import EmailBar from './EmailBar';
+
+class Home extends Component {
+  render() {
+    return (
+        <div className="home">
+          <div className="preview">
+            <h1>Preview</h1>
+            <div className="lesson-container">
+              <LessonCard />
+              <LessonCard />
+              <LessonCard />
+              <LessonCard />
+              <LessonCard />
+              <LessonCard />          
+            </div>
+            <div className="preview-description">
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et ligula ligula. Nullam pharetra sapien eget magna semper accumsan. Pellentesque lobortis tincidunt gravida. Integer iaculis velit in augue pellentesque, vitae cursus nunc laoreet. Vivamus justo nulla, efficitur sed nulla id, tincidunt vehicula lectus. Nunc tempus rhoncus porttitor. Fusce vitae venenatis nunc. Sed ornare scelerisque neque ut euismod. Cras id scelerisque ipsum, eleifend euismod libero. Maecenas dignissim ut diam quis ornare. Duis vehicula vehicula imperdiet.
+              </p>
+            </div>
+          </div>
+          <EmailBar />
+        </div>
+      )
+  }
+}
+
+export default Home;
