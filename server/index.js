6460922e18b828eff3176aa3ecaf32c95ad9a6ca
Andy Tran <transient.tran@gmail.com>
2018-01-30 16:06:58 -0500

    connected to mailchimp

--- server/index.js
+++ server/index.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const Mailchimp = require('mailchimp-api-v3');
+require('dotenv').config();
+
+var mc_api_key = process.env.MAILCHIMP_API_KEY;
+var list_id = process.env.MAILING_LIST_ID;
+
 const app = express();
+const mailchimp = new Mailchimp(mc_api_key);
 const port = process.env.PORT || 9001;
+
+//routes
+app.get('/api/memberList', (req, res) => {
+  mailchimp.get(`/lists/${list_id}/members`)
+  .then(function(results){
+    res.send(results);
+  })
+  .catch(function(err){
+    res.send(err);
+  });
+});
+
+
 app.listen(port);
 console.log(`express app listening on port ${port}`);
+
