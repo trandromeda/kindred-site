0bbecc55799784d040ee62d098839799d2bfb42f
Andy Tran <transient.tran@gmail.com>
2018-01-30 09:55:57 -0500

    003 change navbar on scroll (#3)
    * created a window onscroll function to add and remove css classes to the navbar
    
    * added transition to navbar
    
    * made emailbar font size consistent with scrim

--- src/App.js
+++ src/App.js
@@ -8,27 +8,40 @@ import EmailBar from './components/EmailBar';
 import Footer from './components/Footer';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log('mounted');
+    const nav = document.getElementById("navbar");
+    window.onscroll = function() {
+      if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
+        nav.classList.add('white');
+      } else {
+        nav.classList.remove('white');
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <Landing />
-        <div className="user-preference">
+        <div className="preview">
         <h1>Preview</h1>
-        <div className="lesson-container">
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />          
+          <div className="lesson-container">
+            <LessonCard />
+            <LessonCard />
+            <LessonCard />
+            <LessonCard />
+            <LessonCard />
+            <LessonCard />          
+          </div>
         </div>
 
         <EmailBar />
 
         <Footer />
 
-        </div>
       </div>
     );
   }
