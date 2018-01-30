c794937515c71393d606d5de3e3a20e395fa8956
Andy Tran <transient.tran@gmail.com>
2018-01-30 16:55:16 -0500

    parsing body in server now

--- server/index.js
+++ server/index.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const Mailchimp = require('mailchimp-api-v3');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 var mc_api_key = process.env.MAILCHIMP_API_KEY;
 var list_id = process.env.MAILING_LIST_ID;
 
 const app = express();
+app.use(bodyParser.json());
+
 const mailchimp = new Mailchimp(mc_api_key);
 const port = process.env.PORT || 9001;
 
 //routes
 app.get('/api/memberList', (req, res) => {
   mailchimp.get(`/lists/${list_id}/members`)
-  .then(function(results){
+  .then(function (results){
     res.send(results);
   })
-  .catch(function(err){
+  .catch(function (err){
     res.send(err);
   });
 });
+
+app.post('/api/subscribe', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+
+  // mailchimp.post('/lists/$(list_id)/members/')
+  // .then(function (data) {
+  //   res.send(data)
+  // })
+  // .catch(fundtion (err) {
+  //   res.send(err);
+  // })
+})
 
 
 app.listen(port);
