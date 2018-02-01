46fce4d3f22369d28c226296f2f365851acd801a
Andy Tran <transient.tran@gmail.com>
2018-02-01 15:18:34 -0500

    revisions

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import { ReactTypeformEmbed } from 'react-typeform-embed';
 
 class Contact extends Component {
-  shouldComponentUpdate () {
-    console.log('should');
-  }
-  
-  componentDidMount () {
-    console.log('mount');
-  }
-
   render() {
     return (
         <div className="contact">
