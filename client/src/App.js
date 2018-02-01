4ca535d92c5c5b07e1792889dd06f32de6336e57
Andy Tran <transient.tran@gmail.com>
2018-02-01 11:56:58 -0500

    put navbar scss into its own file and changed the colour to translucent

--- client/src/App.js
+++ client/src/App.js
@@ -16,9 +16,9 @@ class App extends Component {
     const nav = document.getElementById("navbar");
     window.onscroll = function() {
       if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-        nav.classList.add('white');
+        nav.classList.add('scroll');
       } else {
-        nav.classList.remove('white');
+        nav.classList.remove('scroll');
       }
     }
   }
