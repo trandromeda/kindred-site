9b00011c0c2cd5103f451f17b4d6aaac83db3e50
Andy Tran <transient.tran@gmail.com>
2018-02-04 18:08:42 -0500

    icons added

--- client/src/components/HowItWorks.js
+++ client/src/components/HowItWorks.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import SignupSVG from '../images/monitor.svg';
+import SearchSVG from '../images/search.svg';
+import MeetUpSVG from '../images/talking.svg';
 
 class HowItWorks extends Component {
   render() {
@@ -8,15 +10,18 @@ class HowItWorks extends Component {
           <h1 className='title'>How It Works</h1>
           <div className='step'>
             <h2>Setup</h2>
+            <img src={SignupSVG} />
             <p>Create your profile in a few easy steps</p>
           </div>
           <div className='step'>
             <h2>Browse lessons</h2>
+            <img src={SearchSVG} />
             <p>Can't find what you're looking for? Make a request in the marketplace</p>
           </div>
           <div className='step'>
-          <h2>Meet in person!</h2>
-          <p>Have a great learning experience</p>
+            <h2>Meet in person!</h2>
+            <img src={MeetUpSVG} />
+            <p>Have a great learning experience</p>
           </div>                    
         </div>
       )
