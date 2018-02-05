1070747e7d7541098eb15b80f3b96af7c1233b7c
Andy Tran <transient.tran@gmail.com>
2018-02-04 22:14:17 -0500

    added in a description, will need to tweak the copy later

--- client/src/components/HowItWorks.js
+++ client/src/components/HowItWorks.js
@@ -11,18 +11,21 @@ class HowItWorks extends Component {
           <div className='step'>
             <h2>Setup</h2>
             <img src={SignupSVG} />
-            <p>Create your profile in a few easy steps</p>
+            <p>Quickly build your profile with our guided walkthrough</p>
           </div>
           <div className='step'>
             <h2>Browse lessons</h2>
             <img src={SearchSVG} />
-            <p>Can't find what you're looking for? Make a request in the marketplace</p>
+            <p>Can't find what you're looking for? Kindred will create a Lesson Request, bringing in potential teachers</p>
           </div>
           <div className='step'>
-            <h2>Meet in person!</h2>
+            <h2>Learn</h2>
             <img src={MeetUpSVG} />
-            <p>Have a great learning experience</p>
-          </div>                    
+            <p>After getting approved, meet new people for an interactive learning experience</p>
+          </div>
+          <div>
+            <p>We are building out the Kindred prototype to better show you how the learning marketplace works. Sign up for our mailing list to be notified it comes out!</p>
+          </div>                 
         </div>
       )
   }
